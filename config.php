ec40e29f75090e7249c530087a17670ad8e85ce5
Gavoci <101709194+Gavoci@users.noreply.github.com>
2024-01-29 12:53:26 +0100

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'notes');
+define('DB_NAME', 'NoteStudio');
  
 /* Attempt to connect to MySQL database */
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
