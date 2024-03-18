38a7061a9a72e09aa9b674d2fba678a667980d96
Gavoci <101709194+Gavoci@users.noreply.github.com>
2024-03-18 13:24:02 +0100

    Add files via upload

---
+++ api.php
@@ -0,0 +1,39 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "NoteStudio";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+function getTableData($table) {
+    global $conn;
+    $sql = "SELECT * FROM $table";
+    $result = $conn->query($sql);
+
+    $data = array();
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $data[] = $row;
+        }
+    }
+    return $data;
+}
+
+$table = $_GET['table'];
+
+if ($table == 'categories' || $table == 'notes' || $table == 'users') {
+    $responseData = getTableData($table);
+    echo json_encode($responseData);
+} else {
+    echo json_encode(array('error' => 'Invalid table name'));
+}
+
+$conn->close();
+?>
