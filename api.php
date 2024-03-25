4be73527b2ee9d40a47572f53ff432fa924d5fac
Gavoci <101709194+Gavoci@users.noreply.github.com>
2024-03-25 13:10:28 +0100

    Update api.php

--- api.php
+++ api.php
@@ -12,9 +12,14 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-function getTableData($table) {
+function getTableData($table, $idName, $id = null) {
     global $conn;
-    $sql = "SELECT * FROM $table";
+    $idName = $conn->real_escape_string($idName);
+    if ($id !== null) {
+        $sql = "SELECT * FROM $table WHERE $idName = $id";
+    } else {
+        $sql = "SELECT * FROM $table";
+    }
     $result = $conn->query($sql);
 
     $data = array();
@@ -27,9 +32,25 @@ function getTableData($table) {
 }
 
 $table = $_GET['table'];
+$id = isset($_GET['id']) ? $_GET['id'] : null;
+$idName = null;
+
+// Mapping tra i nomi delle tabelle e i nomi degli ID
+$idNamesMap = [
+    'categories' => 'cat_id',
+    'notes' => 'note_id',
+    'users' => 'user_id'
+];
+
+if (array_key_exists($table, $idNamesMap)) {
+    $idName = $idNamesMap[$table];
+} else {
+    echo json_encode(array('error' => 'Invalid table name'));
+    exit;
+}
 
 if ($table == 'categories' || $table == 'notes' || $table == 'users') {
-    $responseData = getTableData($table);
+    $responseData = getTableData($table, $idName, $id);
     $output = array();
     $output['table'] = $table;
     $output['data'] = $responseData;
