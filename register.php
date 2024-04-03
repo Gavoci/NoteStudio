318e8708d4412c19baeebbf3080d2fa1e29a04f2
Gavoci <101709194+Gavoci@users.noreply.github.com>
2024-04-03 12:37:13 +0200

    Update register.php

--- register.php
+++ register.php
@@ -1,8 +1,5 @@
 <?php
-// Initialize the session
 session_start();
-
-// Check if the user is already logged in, if yes then redirect him to welcome page
 if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
     header("location: welcome.php");
     exit;
@@ -10,7 +7,6 @@ if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
 ?>
 <!doctype html>
 <html lang="en">
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -18,11 +14,8 @@ if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
-
     <link rel="stylesheet" href="assets/style.css">
-
 </head>
-
 <body>
     <div class="row align-items-center justify-content-center vh-100 vw-100">
         <div class="col-lg-6 d-none d-md-none d-lg-block">
@@ -43,50 +36,51 @@ if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
                             <form action="php/user_registration.php" method="post" class="row g-3">
                                 <div class="col-12">
                                     <label for="name">Name</label>
-                                    <input type="text" name="name" id="" class="form-control">
+                                    <input type="text" name="name" class="form-control" required>
                                 </div>
                                 <div class="col-12">
                                     <label for="email">Email</label>
-                                    <input type="email" name="email" id="" class="form-control">
+                                    <input type="email" name="email" class="form-control" required>
                                 </div>
                                 <div class="col-12">
-                                    <label for="number">Phone</label>
-                                    <input type="number" name="phone" id="" class="form-control">
+                                    <label for="phone">Phone</label>
+                                    <input type="tel" name="phone" class="form-control" required>
                                 </div>
                                 <div class="col-12">
                                     <label for="age">Age</label>
-                                    <input type="number" name="age" id="" class="form-control">
+                                    <input type="number" name="age" class="form-control" required>
                                 </div>
                                 <div class="col-12">
                                     <label for="gender">Gender</label>
-                                    <select name="gender" class="form-control" id="">
+                                    <select name="gender" class="form-control" required>
                                         <option value="" selected hidden>Choose Gender...</option>
                                         <option value="Male">Male</option>
                                         <option value="Female">Female</option>
                                         <option value="Other">Other</option>
                                     </select>
                                 </div>
-
                                 <div class="col-12">
                                     <label for="password">Password</label>
                                     <div class="input-group">
-                                        <input type="password" name="password" id="passwordInput" class="form-control">
-                                        <button type="button" id="togglePassword" class="btn btn-light border"><i
-                                                class="bi bi-eye"></i></button>
+                                        <input type="password" name="password" class="form-control" required>
+                                        <button type="button" class="btn btn-light border" id="togglePassword">
+                                            <i class="bi bi-eye"></i>
+                                        </button>
                                     </div>
                                 </div>
-
-                                <!-- Add password confirmation field -->
                                 <div class="col-12">
                                     <label for="confirmPassword">Confirm Password</label>
                                     <div class="input-group">
-                                        <input type="password" name="confirmPassword" id="confirmPasswordInput"
-                                            class="form-control">
-                                        <button type="button" id="toggleConfirmPassword"
-                                            class="btn btn-light border"><i class="bi bi-eye"></i></button>
+                                        <input type="password" name="confirmPassword" class="form-control" required>
+                                        <button type="button" class="btn btn-light border" id="toggleConfirmPassword">
+                                            <i class="bi bi-eye"></i>
+                                        </button>
                                     </div>
                                 </div>
-
+                                <div class="col-12">
+                                    <label for="tenant_code">Tenant Code</label>
+                                    <input type="text" name="tenant_code" class="form-control" required>
+                                </div>
                                 <div class="d-grid">
                                     <input type="submit" value="Sign Up" class="btn btn-primary">
                                 </div>
@@ -103,8 +97,8 @@ if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
     </div>
     <script>
         document.addEventListener('DOMContentLoaded', function () {
-            const passwordInput = document.getElementById('passwordInput');
-            const confirmPasswordInput = document.getElementById('confirmPasswordInput');
+            const passwordInput = document.querySelector('input[name="password"]');
+            const confirmPasswordInput = document.querySelector('input[name="confirmPassword"]');
             const togglePassword = document.getElementById('togglePassword');
             const toggleConfirmPassword = document.getElementById('toggleConfirmPassword');
 
@@ -119,22 +113,17 @@ if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
             function togglePasswordVisibility(inputField, toggleButton) {
                 const type = inputField.getAttribute('type') === 'password' ? 'text' : 'password';
                 inputField.setAttribute('type', type);
-
-                // Change the eye icon based on the password visibility
-                toggleButton.innerHTML = type === 'password' ? '<i class="bi bi-eye"></i>' :
-                    '<i class="bi bi-eye-slash"></i>';
+                toggleButton.innerHTML = type === 'password' ? '<i class="bi bi-eye"></i>' : '<i class="bi bi-eye-slash"></i>';
             }
         });
     </script>
     <script>
-        // Automatically hide the alert after 5 seconds
         setTimeout(function () {
             document.getElementById('alertMessage').style.display = 'none';
-        }, 5000); // 5000 milliseconds = 5 seconds
+        }, 5000);
     </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
     </script>
 </body>
-
 </html>
