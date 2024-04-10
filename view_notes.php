d1ab81670b7a5acbe93c31a037754fae23cbb342
Gavoci <101709194+Gavoci@users.noreply.github.com>
2024-04-10 20:43:03 +0200

    Update view_notes.php

--- view_notes.php
+++ view_notes.php
@@ -10,10 +10,7 @@ if ($ViewNotesDetailsresult->num_rows > 0) {
         $noteUser = $ViewNotesDetailsrow['note_user'];
         $title = $ViewNotesDetailsrow['note_title'];
         $catName = $ViewNotesDetailsrow['cat_name'];
-        $tags = $ViewNotesDetailsrow['note_tags'];
         $desc = $ViewNotesDetailsrow['note_desc'];
-        $privacy = $ViewNotesDetailsrow['note_privacy'];
-        $image = $ViewNotesDetailsrow['note_image'];
         $date = date("d M Y", strtotime($ViewNotesDetailsrow['note_date']));
         ?>
         <main>
@@ -21,14 +18,6 @@ if ($ViewNotesDetailsresult->num_rows > 0) {
                 <div class="row g-3">
                     <div class="col-lg-8 col-md-6">
                         <div class="card">
-                            <?php
-                            $image = $ViewNotesDetailsrow['note_image'];
-
-                            if (!empty($image)) {
-                                // If $image is not empty (not null or empty string), display the image
-                                echo '<img src="assets/notesImages/' . $image . '" alt="Note Image" class="notesImage">';
-                            }
-                            ?>
                             <div class="card-body">
                                 <h5 class="fw-bold text-capitalize display-6 my-4">Title :
                                     <?= $title ?>
@@ -42,31 +31,12 @@ if ($ViewNotesDetailsresult->num_rows > 0) {
                                 <div class="card-text">
                                     <?= $desc ?>
                                 </div>
-                                <?php
-                                // Assuming $tags contains fetched tags from the database
-                                $tagsArray = preg_split('/,\s*/', $tags); // Split tags into an array by commas and optional spaces
-                        
-                                // Filter out empty tags (if any)
-                                $tagsArray = array_filter($tagsArray, function ($value) {
-                                    return !empty(trim($value));
-                                });
-
-                                $tagString = '#' . implode(' #', $tagsArray); // Add hashtags and join tags without commas
-                        
-                                echo '<p class="text-secondary"><i class="bi bi-tag-fill"></i> Tags: ' . $tagString . '</p>'; // Display the tags with hashtags without commas
-                                ?>
-
-
-
-
                             </div>
                         </div>
                         <?php
-                         include 'widgets/authorCard.php';
-                        include 'widgets/recentNotes.php'; ?>
+                         include 'widgets/authorCard.php'; ?>
                     </div>
                     <div class="col-lg-4 col-md-6">
-                    <?php include 'widgets/categories_widget.php'; ?>
                     </div>
                 </div>
 
