a6f5d4174bdbb32e4be7b98149d46701aef421c6
Gavoci <101709194+Gavoci@users.noreply.github.com>
2024-04-12 18:56:37 +0200

    Update tenant_list.php

--- tenant_list.php
+++ tenant_list.php
@@ -1,44 +1,57 @@
-<?php include 'partials/header.php'; ?>
-<?php include 'partials/navbar.php'; ?>
+<?php 
+include 'partials/header.php';
+include 'partials/navbar.php'; 
 
-<main>
-    <div class="container my-4">
-        <h6 class="fw-bold my-3">Tenants</h6>
-        <ul class="list-group list-group-numbered">
-            <?php
-            $FetchTenantssql = "SELECT * FROM tenants ORDER BY `tenant_name` ASC";
-            $FetchTenantsresult = $conn->query($FetchTenantssql);
+$user_id = $_SESSION['user_id'];
 
-            if ($FetchTenantsresult->num_rows > 0) {
-                // Output data of each row
-                while ($row = $FetchTenantsresult->fetch_assoc()) {
-                    $tenantCode = $row["tenant_code"];
-                    $tenantName = $row['tenant_name'];
+// Query per ottenere i tenant
+$FetchTenantsDetailssql = "SELECT * FROM tenants";
+$FetchTenantsDetailsresult = $conn->query($FetchTenantsDetailssql);
+
+if ($FetchTenantsDetailsresult->num_rows > 0) {
+    ?>
+    <main>
+        <style>
+            .tenant-name:hover {
+                text-decoration: underline;
+            }
+        </style>
+        <div class="container my-4">
+            <h5 class="fw-bold">Tenants List</h5>
+            <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3">
+                <?php
+                while ($FetchTenantsDetailsrow = $FetchTenantsDetailsresult->fetch_assoc()) {
+                    // set data 
+                    $tenant_code = $FetchTenantsDetailsrow['tenant_code'];
+                    $tenant_name = $FetchTenantsDetailsrow['tenant_name'];
                     ?>
-                    <li class="list-group-item p-3 d-flex justify-content-between align-items-start">
-                        <div class="ms-2 me-auto">
-                            <div class="fw-bold text-capitalize">
-                                <?= $tenantName ?>
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-body">
+                                <p class="text-secondary mb-0 fs-14 text-capitalize">
+                                    <i class="bi bi-building"></i>
+                                    <span class="fw-bold tenant-name"><?= $tenant_name ?></span>
+                                </p>
+                                 <!-- Visualizza il tenant code sotto il nome del tenant -->
+                                 <p class="text-secondary fs-14 mb-0">
+                                    <span class="fw-bold">Tenant Code:</span> <?= $tenant_code ?>
+                                </p>
                             </div>
                         </div>
-                        <span class="badge bg-primary rounded-pill">
-                            <?= $tenantCode ?>
-                        </span>
-                    </li>
-                    <?php
-                }
-            } else {
-                ?>
-                <li class="list-group-item d-flex justify-content-between align-items-start">
-                    <div class="ms-2 me-auto">
-                        <div class="fw-bold">No Tenants Found</div>
                     </div>
-                </li>
-                <?php
-            }
-            ?>
-        </ul>  
-    </div>
-</main>
-
+                <?php } ?>
+            </div>
+        </div>
+    </main>
+<?php } else { ?>
+    <main>
+        <div class="container my-4">
+            <div class="card rounded-4 border shadow-sm">
+                <div class="card-body text-center">
+                    <h5 class="fw-bold">No Tenants record found!</h5>
+                </div>
+            </div>
+        </div>
+    </main>
+<?php } ?>
 <?php include 'partials/javascripts.php'; ?>
