3cfb115f467e267c2ccef94fbf5d7215401c75e3
Gavoci <101709194+Gavoci@users.noreply.github.com>
2024-04-12 23:21:09 +0200

    Update super_notes_list.php

--- super_notes_list.php
+++ super_notes_list.php
@@ -7,116 +7,100 @@ $FetchNotesDetailssql = "SELECT * FROM notes";
 $FetchNotesDetailsresult = $conn->query($FetchNotesDetailssql);
 
 if ($FetchNotesDetailsresult->num_rows > 0) {
-    ?>
-    <main>
-        <div class="container my-4">
-            <h5 class="fw-bold">Notes List</h5>
-            <div class="row g-3">
-                <?php
-                while ($FetchNotesDetailsrow = $FetchNotesDetailsresult->fetch_assoc()) {
-                    // set data 
-                    $id = $FetchNotesDetailsrow['note_id'];
-                    $title = $FetchNotesDetailsrow['note_title'];
-                    $views = $FetchNotesDetailsrow['note_views'];
-                    $date = date("d M Y", strtotime($FetchNotesDetailsrow['note_date']));
-                    $tenant_code = $FetchNotesDetailsrow['tenant_code']; // Codice tenant
+?>
+<main>
+    <div class="container my-4">
+        <h5 class="fw-bold">Notes Grid</h5>
+        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3">
+            <?php
+            while ($FetchNotesDetailsrow = $FetchNotesDetailsresult->fetch_assoc()) {
+                // set data 
+                $id = $FetchNotesDetailsrow['note_id'];
+                $title = $FetchNotesDetailsrow['note_title'];
+                $views = $FetchNotesDetailsrow['note_views'];
+                $date = date("d M Y", strtotime($FetchNotesDetailsrow['note_date']));
+                $tenant_code = $FetchNotesDetailsrow['tenant_code']; // Codice tenant
 
-                    ?>
-                    <div class="col-12">
-                        <div class="card">
-                            <div class="card-body">
+                ?>
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body d-flex justify-content-between">
+                            <div>
                                 <div class="row align-items-center">
-                                    <div class="col-md-12 col-lg-1">
+                                    <div class="col-12 col-md-4">
                                         <img src="assets/img/book1.png" alt="">
                                     </div>
-                                    <div class="col-md-12 col-lg-7">
+                                    <div class="col-12 col-md-8">
                                         <a href="php/note_views.php?noteId=<?= $id ?>" class="notesTitleLink">
                                             <h6 class="fw-bold mb-0 text-capitalize">
                                                 <?= $title ?>
                                             </h6>
                                         </a>
-                                        <?php
-                                        if ($views > 0) {
-                                            ?>
+                                        <?php if ($views > 0) { ?>
                                             <p class="text-secondary fs-14 mb-0"><i class="bi bi-eye-fill"></i>
                                                 <?= $views ?>
                                                 Views
                                             </p>
-                                            <?php
-                                        }
-                                        ?>
+                                        <?php } ?>
                                         <p class="text-secondary fs-14 mb-0">
                                             <?= $date ?>
                                         </p>
-                                        <p class="text-secondary fs-14 mb-0"><strong>Tenant Code:</strong> <?= $tenant_code ?></p> <!-- Codice tenant -->
-                                    </div>
-                                    <div class="col-lg-4 d-none d-lg-block">
-                                        <!-- Dropdown per le azioni -->
-                                        <div class="dropdown">
-                                            <button class="btn btn-light border btn-sm dropdown-toggle" type="button"
-                                                data-bs-toggle="dropdown" aria-expanded="false">
-                                                Action
-                                            </button>
-                                            <ul class="dropdown-menu">
-                                                <li><a class="dropdown-item" href="edit_notes.php?noteId=<?=$id?>"><i class="bi bi-pencil-square me-2"></i> Edit</a></li>
-                                                <li><a class="dropdown-item" href="#" data-bs-toggle="modal"
-                                                        data-bs-target="#deleteModal-<?= $id ?>"><i class="bi bi-trash me-2"></i> Delete</a></li>
-                                            </ul>
+                                        <div class="d-flex align-items-center" style="width: 100%"> <!-- Aggiunto lo stile per la larghezza fissa -->
+                                            <p class="text-secondary fs-14 mb-0 me-2" style="white-space: nowrap;"><strong>Tenant Code:</strong></p> <!-- Codice tenant -->
+                                            <p class="text-secondary fs-14 mb-0" style="white-space: nowrap;"><?= $tenant_code ?></p> <!-- Imposta il tenant code per non andare a capo -->
                                         </div>
                                     </div>
-                                    <!-- delete modal  -->
-                                    <div class="modal fade" id="deleteModal-<?= $id ?>" tabindex="-1"
-                                        aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                        <div class="modal-dialog modal-dialog-centered">
-                                            <div class="modal-content">
-                                                <div class="modal-header">
-                                                    <h1 class="modal-title fs-5 fw-bold" id="exampleModalLabel"> <i
-                                                            class="bi bi-exclamation-triangle-fill"></i> Are you sure ? </h1>
-                                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                                        aria-label="Close"></button>
-                                                </div>
-                                                <div class="modal-body">
-                                                    This action <b>CANNOT</b> be undone. This will permanently delete the <b>
-                                                        <?= $title ?>
-                                                    </b>.
-                                                </div>
-                                                <div class="modal-footer">
-                                                    <button type="button" class="btn btn-light border"
-                                                        data-bs-dismiss="modal">No, Keep it.</button>
-                                                    <form action="php/delete_note.php" method="post">
-                                                        <input type="hidden" name="noteId" value="<?= $id ?>">
-                                                        <input type="hidden" name="status" value="1">
-                                                        <button type="submit" class="btn btn-danger" name="delete_note">Yes,
-                                                            Delete <i class="bi bi-trash"></i></button>
-                                                    </form>
-
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <!-- delete modal end  -->
-
                                 </div>
                             </div>
+                            <!-- Dropdown per le azioni -->
+                            <div class="dropdown">
+                                <button class="btn btn-light border btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <i class="bi bi-chevron-down"></i>
+                                </button>
+                                <ul class="dropdown-menu dropdown-menu-end">
+                                    <li><a class="dropdown-item" href="edit_notes.php?noteId=<?= $id ?>"><i class="bi bi-pencil-square me-2"></i> Edit</a></li>
+                                    <li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#deleteModal-<?= $id ?>"><i class="bi bi-trash me-2"></i> Delete</a></li>
+                                </ul>
+                            </div>
+                            <!-- delete modal  -->
+                            <div class="modal fade" id="deleteModal-<?= $id ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-centered">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h1 class="modal-title fs-5 fw-bold" id="exampleModalLabel"> <i class="bi bi-exclamation-triangle-fill"></i> Are you sure ? </h1>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            This action <b>CANNOT</b> be undone. This will permanently delete the <b><?= $title ?></b>.
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-light border" data-bs-dismiss="modal">No, Keep it.</button>
+                                            <form action="php/delete_note.php" method="post">
+                                                <input type="hidden" name="noteId" value="<?= $id ?>">
+                                                <input type="hidden" name="status" value="1">
+                                                <button type="submit" class="btn btn-danger" name="delete_note">Yes, Delete <i class="bi bi-trash"></i></button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- delete modal end  -->
                         </div>
                     </div>
-                <?php } ?>
-
+                </div>
+            <?php } ?>
+        </div>
+    </div>
+</main>
+<?php } else { ?>
+<main>
+    <div class="container my-4">
+        <div class="card rounded-4 border shadow-sm">
+            <div class="card-body text-center">
+                <h5 class="fw-bold">No Notes record found !</h5>
             </div>
         </div>
-    </main>
-
-    <?php } else { ?>
-        <main>
-            <div class="container my-4">
-                <div class="card rounded-4 border shadow-sm">
-                    <div class="card-body text-center">
-                        <h5 class="fw-bold">No Notes record found !</h5>
-                    </div>
-                </div>
-            </div>
-        </main>
-    <?php
-    }
-?>
+    </div>
+</main>
+<?php } ?>
 <?php include 'partials/javascripts.php'; ?>
