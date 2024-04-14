8f095487cdb9f230a4aeefc26e91bf99dac30e9b
Gavoci <101709194+Gavoci@users.noreply.github.com>
2024-04-14 23:38:02 +0200

    Update super_notes_list.php

--- super_notes_list.php
+++ super_notes_list.php
@@ -11,6 +11,7 @@ if ($FetchNotesDetailsresult->num_rows > 0) {
 <main>
     <div class="container my-4">
         <h5 class="fw-bold">Notes Grid</h5>
+        <?php include 'widgets/alert_message.php'; ?>
         <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3">
             <?php
             while ($FetchNotesDetailsrow = $FetchNotesDetailsresult->fetch_assoc()) {
