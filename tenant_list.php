3d46e861c08f8fdc637d660d3dbdf4ec34247f5d
Gavoci <101709194+Gavoci@users.noreply.github.com>
2024-04-14 23:38:28 +0200

    Update tenant_list.php

--- tenant_list.php
+++ tenant_list.php
@@ -18,6 +18,7 @@ if ($FetchTenantsDetailsresult->num_rows > 0) {
         </style>
         <div class="container my-4">
             <h5 class="fw-bold">Tenants List</h5>
+            <?php include 'widgets/alert_message.php'; ?>
             <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3">
                 <?php
                 while ($FetchTenantsDetailsrow = $FetchTenantsDetailsresult->fetch_assoc()) {
